e531510f8628032dcc3a4318f3349951d47758f5
Fallah Andy Prakasa <me@flla.my.id>
2023-09-07 23:03:35 +0700

    feat: initial configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .state("name", "flla-api")
+  .state("version", 1)
+  .state("author", "Fallah Andy Prakasa")
+  .state("description", "It's just an API")
+  .get("/", () => ({ msg: "Hello, Elysia!" }))
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
